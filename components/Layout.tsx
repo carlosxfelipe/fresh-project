6562345682c6a831f801eb0488752881a19c8b90
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-07-21 16:11:01 -0300

    feat: implement Layout and Navbar components for improved structure

---
+++ components/Layout.tsx
@@ -0,0 +1,14 @@
+import { ComponentChildren } from "preact";
+import Navbar from "./Navbar.tsx";
+
+export default function Layout({ children }: { children: ComponentChildren }) {
+  return (
+    <>
+      <Navbar />
+      <main class="px-4 py-12 mx-auto">
+        {children}
+      </main>
+      {/* Futuro Footer */}
+    </>
+  );
+}
